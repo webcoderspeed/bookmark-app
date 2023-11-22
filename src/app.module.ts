001e0710f2cbf822a3a0f77059cd8d1eca5279fd
webcoderspeed <webcoderspeed@gmail.com>
2023-11-22 16:49:40 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [UserModule, BookmarkModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
